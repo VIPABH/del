f0e2d499205515aa3aeeeb6a9984d449e1c69f91
VIPABH <a891983.2@gmail.com>
2025-03-09 17:19:11 +0300

    Update d.py

--- d.py
+++ d.py
@@ -40,7 +40,7 @@ async def delete_filtered_messages():
         print(f"حدث خطأ أثناء الحذف: {str(e)}")
 
 scheduler = AsyncIOScheduler()
-scheduler.add_job(delete_filtered_messages, 'interval', hours=10)
+scheduler.add_job(delete_filtered_messages, 'interval', seconds=10)
 scheduler.start()
 
 ABH.start()

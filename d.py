e8e5118fb0d2bba1d2895185eee5e963bb2cec11
VIPABH <a891983.2@gmail.com>
2025-03-10 02:43:25 +0300

    Update d.py

--- d.py
+++ d.py
@@ -23,6 +23,17 @@ ABH = TelegramClient("ubot", api_id, api_hash)
 plugin_category = "extra"
 excluded_user_ids = [793977288, 1421907917, 7308514832, 6387632922, 7908156943]
 
+delete_count = {
+    "الملفات": 0,
+    "الصور": 0,
+    "الفيديوهات": 0,
+    "المتحركات (GIF)": 0,
+    "الملفات الصوتية": 0,
+    "الرسائل الصوتية المرئية": 0,
+    "الروابط": 0,
+    "الملصقات": 0
+}
+
 async def delete_filtered_messages():
     chat_id = -1001996913931
 
@@ -42,6 +53,7 @@ async def delete_filtered_messages():
                 if message.sender_id in excluded_user_ids:
                     continue
                 await message.delete()
+                delete_count[msg_type] += 1 
                 print(f"تم حذف رسالة من النوع {msg_type}")
 
         async for message in ABH.iter_messages(chat_id):
@@ -49,6 +61,7 @@ async def delete_filtered_messages():
                 if message.sender_id in excluded_user_ids:
                     continue
                 await message.delete()
+                delete_count["الملصقات"] += 1
                 print("تم حذف ملصق (Sticker)")
 
     except Exception as e:
@@ -56,20 +69,20 @@ async def delete_filtered_messages():
 
 scheduler = AsyncIOScheduler()
 scheduler.add_job(delete_filtered_messages, 'interval', minutes=5)
-uid = [
-    1910015590
-]
+
+uid = [1910015590]
+
 @ABH.on(events.NewMessage(pattern="امسح$"))
 async def delete_on_command(event):
     id = event.sender_id
     if id in uid: 
         await delete_filtered_messages()
-        await event.reply("تم الحذف بنجاح!")
+        
+        report = "\n".join([f"{msg_type}: {count} رسالة" for msg_type, count in delete_count.items() if count > 0])
+        await event.reply(f"تم الحذف بنجاح!\n\nتقرير الحذف:\n{report}")
     else:
-        await event.reply("صديقي الامر خاص بالمشرفين , خلي ابن هاشم يضيفك بي ``ههههه``")
-
-    await delete_filtered_messages()
-    await event.reply("تم الحذف بنجاح!")
+        await event.reply("صديقي الامر خاص بالمشرفين , خلي ابن هاشم يضيفك بي ```ههههه```")
+        return
 
 async def main():
     await ABH.start()

cc2ca2e7d1087d96711f62a0aaed9bfd4f08f8ac
VIPABH <a891983.2@gmail.com>
2025-03-21 17:49:56 +0300

    Update d.p

--- d.py
+++ d.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, events
 from telethon.tl.functions.messages import AddChatUserRequest
-
+import os
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 

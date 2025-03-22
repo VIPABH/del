d4e156548347ee44666b1d2dc0bb00a678c606ab
VIPABH <a891983.2@gmail.com>
2025-03-22 18:30:03 +0300

    Update __init__.py

--- config.py/__init__.py
+++ config.py/__init__.py
@@ -1,3 +1,3 @@
-from config.abh import start_bot
+from .config.abh import start_bot
 from config.r import reply_to_messages  # استيراد الرد على الرسائل
 from config.r2 import handle_commands  # استيراد إدارة الأوامر
